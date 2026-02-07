145328b0ee8b999f29d6daab0892952900fcd700
MenaceHecker <thedynamic2508@gmail.com>
2026-02-06 19:47:36 -0500

    Updated spacing in between sections

--- components/Section.tsx
+++ components/Section.tsx
@@ -8,10 +8,9 @@ export default function Section({
   return (
     <section
       id={id}
-      className="relative px-6 py-20 sm:py-28 md:py-32"
-      style={{ scrollMarginTop: 96 }}
+      className="scroll-mt-24 py-20 md:py-24"
     >
-      <div className="mx-auto w-full max-w-6xl">{children}</div>
+      {children}
     </section>
   );
 }
